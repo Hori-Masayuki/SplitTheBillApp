ea006141d428fda3b324708a0e32a07a5f12e6bf
Hori-Masayuki <hori.masayuki1132@gmail.com>
2021-05-09 14:12:37 +0900

    MediaPlayerの追加
    音源の再生

--- app/src/main/java/xyz/takablog/splitthebillapp/MainActivity.java
+++ app/src/main/java/xyz/takablog/splitthebillapp/MainActivity.java
@@ -1,14 +1,35 @@
 package xyz.takablog.splitthebillapp;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        音楽を鳴らす
+        player = MediaPlayer.create(this, R.raw.sound4);
+        player.start();
+    }
+
+//    セッティング画面へ遷移
+    public void tapStart(View view) {
+        Intent intent = new Intent(this,SettingActivity.class);
+        startActivity(intent);
+    }
+
+//    遊び方画面へ遷移
+    public void tapHowTo(View view) {
+        Intent intent = new Intent(this, HowToActivity.class);
+        startActivity(intent);
     }
 }
